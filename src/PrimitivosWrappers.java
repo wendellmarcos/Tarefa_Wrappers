f8ceccca2a7cc5ae37b33602aa3f12458a083fbc
Wendell Marcos <wendell.marcos.code@gmail.com>
2022-09-15 03:46:30 -0300

    Tarefa_Wrapper

---
+++ src/PrimitivosWrappers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PrimitivosWrappers {
+
+        public static void main(String [] args){
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Digite um numero! ");
+            long number1 = sc.nextInt();
+            Long number2 = number1;
+            System.out.println("Você digitou: "+ number1);
+
+
+
+
+
+
+            sc.close();
+        }
+}
